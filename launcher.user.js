73ee0e3ede11b2f68d8713b909a67971b8ef8571
zuka72 <king_man72@hotmail.com>
2015-08-15 13:56:27 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1305,7 +1305,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["dfgdfgdfgdfgdfgdfg"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
